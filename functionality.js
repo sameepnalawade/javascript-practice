77b82e7cafe45eeb30cc638529af65cab7c349d7
sameepnalawade <sameep.nalawade97@gmail.com>
2022-11-09 10:25:54 +0530

    asach

--- functionality.js
+++ functionality.js
@@ -1,8 +1,11 @@
+
 function doSomething(){
     const length=document.getElementById('length');
     const breadth=document.getElementById('breadth');
    
+    console.log(typeof RectangleArea);
     let rectArea=new RectangleArea(+(length.value),+(breadth.value));
+    console.log(typeof rectArea);
     console.log(rectArea.dimensions);
     console.log(rectArea.calcArea());
     rectArea.dimensions={length:10,breadth:10};
